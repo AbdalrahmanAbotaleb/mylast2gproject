0dfa21ec342c296f4e13f5625f01f243148bb2a1
AbdalrahmanAbotaleb <abdalrahmanabotaleb362@gmail.com>
2024-05-30 23:58:19 +0300

    make update in scanhome

--- lib/src/features/ScanningHome/presentation/pages/home.dart
+++ lib/src/features/ScanningHome/presentation/pages/home.dart
@@ -9,136 +9,149 @@ class ScanPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.teal[300],
-        title: Text(
-          'Corn Detection',
-          style: GoogleFonts.aclonica(),
-        ),
-      ),
-      body: Obx(
-        () => SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              controller.loading
-                  ? Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Container(
-                        height: 300,
-                        width: 250,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: Colors.grey,
-                        ),
-                        child: const Center(child: Text('Image Appears here')),
-                      ),
-                    )
-                  : Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Container(
-                        child: Column(
-                          children: [
-                            Container(
-                              height: 280,
-                              width: 220,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(25),
-                              ),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(25),
-                                child: Image.file(controller.image!),
-                              ),
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(backgroundColor: const Color(0xff2c2e29),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0,50,0,0),
+          child: Obx(
+            () => SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  controller.loading
+                      ? Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Container(
+                            height: height * 0.4,
+                            width: width * 0.9,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: const DecorationImage(
+                                  fit: BoxFit.contain,
+                                  image: AssetImage('assets/images/1.png')),
                             ),
-                            const SizedBox(height: 10),
-                            if (controller.diagnosisList.isNotEmpty)
-                              Text(
-                                controller.diagnosisList.last['disease'],
-                                style: GoogleFonts.robotoCondensed(
-                                  fontSize: 20,
+                            // child: const Center(child: Text('Image Appears here')),
+                          ),
+                        )
+                      : Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 280,
+                                width: 220,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(25),
+                                  child: Image.file(controller.image!),
                                 ),
                               ),
-                          ],
-                        ),
-                      ),
-                    ),
-              const SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Container(
-                  height: 50,
-                  width: double.infinity,
-                  child: MaterialButton(
-                    onPressed: controller.loadImageCamera,
-                    child: const Text('Capture Image'),
-                    color: const Color(0xff01AEBC),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Container(
-                  height: 50,
-                  width: double.infinity,
-                  child: MaterialButton(
-                    onPressed: controller.loadImageGallery,
-                    child: const Text('Pick Image from Gallery'),
-                    color: const Color(0xff01AEBC),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Container(
-                height: 150,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: controller.diagnosisList.length,
-                  itemBuilder: (context, index) {
-                    final Key itemKey = UniqueKey();
-                    return GestureDetector(
-                      onTap: () {
-                        Get.to(() => DiagnosisDetailsPage(
-                              image: controller.diagnosisList[index]['image'],
-                              disease: controller.diagnosisList[index]['disease'],
-                            ));
-                      },
-                      child: ListTile(
-                        key: itemKey,
-                        leading: Image.file(
-                          controller.diagnosisList[index]['image'],
-                          width: 50,
-                          height: 50,
-                          fit: BoxFit.cover,
-                        ),
-                        title: Text(
-                          controller.diagnosisList[index]['disease'],
-                          style: const TextStyle(
-                            fontSize: 18,
+                              const SizedBox(height: 10),
+                              if (controller.diagnosisList.isNotEmpty)
+                                Text(
+                                  controller.diagnosisList.last['disease'],
+                                  style: GoogleFonts.robotoCondensed(
+                                    fontSize: 20,
+                                  ),
+                                ),
+                            ],
                           ),
                         ),
-                        trailing: IconButton(
-                          icon: const Icon(Icons.delete),
-                          onPressed: () {
-                            controller.removeDiagnosis(index);
-                          },
+                  const SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      height: 50,
+                      width: double.infinity,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+                        child: MaterialButton(
+                          onPressed: controller.loadImageCamera,
+                          color: const Color(0xff569033),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: const Text(
+                            'Capture Image',
+                            style: TextStyle(color: Color(0xffffffff)),
+                          ),
                         ),
                       ),
-                    );
-                  },
-                ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      height: 50,
+                      width: double.infinity,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+                        child: MaterialButton(
+                          onPressed: controller.loadImageGallery,
+                          color: const Color(0xff569033),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: const Text(
+                            'Pick Image from Gallery',
+                            style: TextStyle(color: Color(0xffffffff)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    height: 150,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: controller.diagnosisList.length,
+                      itemBuilder: (context, index) {
+                        final Key itemKey = UniqueKey();
+                        return GestureDetector(
+                          onTap: () {
+                            Get.to(() => DiagnosisDetailsPage(
+                                  image: controller.diagnosisList[index]['image'],
+                                  disease: controller.diagnosisList[index]
+                                      ['disease'],
+                                ));
+                          },
+                          child: ListTile(
+                            key: itemKey,
+                            leading: Image.file(
+                              controller.diagnosisList[index]['image'],
+                              width: 50,
+                              height: 50,
+                              fit: BoxFit.cover,
+                            ),
+                            title: Text(
+                              controller.diagnosisList[index]['disease'],
+                              style: const TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                            trailing: IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () {
+                                controller.removeDiagnosis(index);
+                              },
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  )
+                ],
               ),
-              const SizedBox(
-                height: 50,
-              )
-            ],
+            ),
           ),
         ),
       ),
